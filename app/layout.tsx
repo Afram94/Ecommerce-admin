83a5dd4cce2e6d5e11ae3968f51390c236ce697b
Afram94 <afram.h@hotmail.com>
2023-10-03 11:24:49 +0200

    Resolved merge conflicts

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
 import { ModalProvider } from '@/providers/modal-provider'
+import prismadb from '@/lib/prismadb'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  
   return (
     <ClerkProvider>
       <html lang="en">
